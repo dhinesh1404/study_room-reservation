436abc78065e40dc53d8965ead80f62ba1b4e16f
dhinesh kumar <dhineshksrinivasan@gmail.com>
2026-03-23 21:29:58 +0900

    new docker files

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("mysql", "user", "password", "mydb");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
+?>
